4336b8e3a8780a3023b352ed67b131bd48a9d558
Chen Xiongjie <chen.xcmail@gmail.com>
2019-03-22 10:49:47 +0100

    Added: idea to remove fields attribute in content, but to access directly

--- src/base/content/Content.go
+++ src/base/content/Content.go
@@ -1,10 +1,14 @@
 package dm/content
 
+/**
+This is a parent struct which consits of location and the content itself(eg. article).
+*/
+
 type Content struct{
   id int,
   content_id int,
   parent_id int,
-  type string,
+  content_type string,
   name string,
-  fields map[string]Field
+  fields map[string]Field //can we remove the fields and article.title directly?
 }
