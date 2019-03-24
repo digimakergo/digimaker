6694d95cb6434aed4c982a4859c8a7cbe5a80400
Chen Xiongjie <chen.xcmail@gmail.com>
2019-03-24 21:03:45 +0100

    Updated: doc

--- utils/go.go
+++ utils/go.go
@@ -2,7 +2,7 @@ package utils
 
 // This is utils for enhace go language.
 
-//IfElse implements ternary. Equvelant to <cond>?trueV:falseV in other language.
+//IfElse implements ternary. Equvelant to <cond>?<trueV>:<falseV> in other language.
 //Ref: https://github.com/golang/go/issues/20774 The reason was insane.
 //
 //There might be performance not perfect, since it needs type convertion.
