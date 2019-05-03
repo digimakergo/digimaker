d3925081a61f9522b92bff7dff937164a7ac2e8b
Chen Xiongjie <chen.xcmail@gmail.com>
2019-05-03 17:03:43 +0200

    Updated: style

--- web/javascript/dmdebug.js
+++ web/javascript/dmdebug.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     html = "<div class='debug'>"+
-        "<span class='title'>Time Spent</span>"+
+        "<a href='#' class='title' title='More debug info'><i class='fas fa-chevron-up'></i> <span>Time Spent</span></a>"+
         "<span>Total: </span><span>"+dmtime.total+"ms</span> "+
         "<span>Query: </span><span>"+dmtime.query+"ms</span> "+
         "<span>Template: </span><span>"+dmtime.template+"ms</span>"+
