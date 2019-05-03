1222ef4b5b03249d3ed39d43b10e64df156dbfa6
Chen Xiongjie <chen@digimaker.no>
2019-05-03 17:13:03 +0200

    Updated: style

--- web/javascript/dmdebug.js
+++ web/javascript/dmdebug.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
     html = "<div class='debug'>"+
-        "<a href='#' class='title' title='More debug info'><i class='fas fa-chevron-up'></i> <span>Time Spent</span></a>"+
+        "<div><a href='#' class='title' title='More debug info'><i class='fas fa-chevron-up'></i> <span>Time Spent</span></a>"+
         "<span>Total: </span><span>"+dmtime.total+"ms</span> "+
         "<span>Query: </span><span>"+dmtime.query+"ms</span> "+
-        "<span>Template: </span><span>"+dmtime.template+"ms</span>"+
+        "<span>Template: </span><span>"+dmtime.template+"ms</span></div>"+
         "</div>";
     $("body").append( html );
 });
