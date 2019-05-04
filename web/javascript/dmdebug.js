9573c5cb4e71ba30542f184fdb1d5a1442cb8d62
Chen Xiongjie <chen.xcmail@gmail.com>
2019-05-04 19:53:44 +0200

    Updated: style

--- web/javascript/dmdebug.js
+++ web/javascript/dmdebug.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-
-    html = "<div class='debug'>"+
-        "<div><a href='#' class='title' title='More debug info'><i class='fas fa-chevron-up'></i> <span>Time Spent</span></a>"+
-        "<span>Total: </span><span>"+dmtime.total+"ms</span> "+
-        "<span>Query: </span><span>"+dmtime.query+"ms</span> "+
-        "<span>Template: </span><span>"+dmtime.template+"ms</span></div>"+
-        "</div>";
+    html = "<div class='debug'>";
     if( errorLog != "" )
     {
         html += "<div class='error'>"+errorLog+"</div>";
     }
+    html += "<div class='time'><a href='#' class='title' title='More debug info'><i class='fas fa-chevron-up'></i> <span>Time Spent</span></a>"+
+            "<span>Total: </span><span>"+dmtime.total+"ms</span> "+
+            "<span>Query: </span><span>"+dmtime.query+"ms</span> "+
+            "<span>Template: </span><span>"+dmtime.template+"ms</span></div>";
+
+    html += "</div>";
     $("body").append( html );
 });
