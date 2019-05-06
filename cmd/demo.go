3036773d9e9856b2e8fb0351f912aad8312afabe
Chen Xiongjie <chen.xcmail@gmail.com>
2019-05-06 23:09:58 +0200

    Fixed: new content in demo didn't set context

--- cmd/demo.go
+++ cmd/demo.go
@@ -131,7 +131,7 @@ func New(w http.ResponseWriter, r *http.Request, vars map[string]string) {
 			}
 		}
 		contentType := r.PostFormValue("type")
-		handler := handler.ContentHandler{}
+		handler := handler.ContentHandler{Context: r.Context()}
 		success, result, error := handler.Create(contentType, params, parentID)
 		fmt.Println(success, result, error)
 		if !success {
