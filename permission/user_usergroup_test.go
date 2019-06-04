03325c6c6dda3053807f35bba367abc820f3c43c
Chen Xiongjie <chen.xcmail@gmail.com>
2019-06-04 23:22:20 +0200

    continue

--- permission/user_usergroup_test.go
+++ permission/user_usergroup_test.go
@@ -4,7 +4,6 @@ import (
 	"dm/contenttype"
 	"dm/fieldtype"
 	"dm/handler"
-	"dm/query"
 	"fmt"
 	"testing"
 )
@@ -15,7 +14,7 @@ func TestUserPermission(m *testing.T) {
 	err := LoadPolicies()
 	fmt.Println(err)
 
-	anonUser, err := handler.Querier().Fetch("user", query.Cond("location.id", 96))
+	anonUser, err := handler.Querier().FetchByID(96)
 
 	policyList, err := GetUserPermission(anonUser.GetCID())
 	fmt.Println(policyList)
