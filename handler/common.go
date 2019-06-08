fe091a2248091aa89e1a0231b2ca3a06c49bc701
Chen Xiongjie <chen.xcmail@gmail.com>
2019-06-08 10:57:41 +0200

    Updated: continue previous

--- handler/common.go
+++ handler/common.go
@@ -15,26 +15,22 @@ import (
 	"github.com/pkg/errors"
 )
 
+//If the user has acccess given matchedData(realData here)
 func HasAccessTo(userID int, module string, action string, realData map[string]interface{}, context context.Context) (bool, error) {
+	//get permission limits
 	policyList, err := permission.GetUserPolicies(userID)
 	debug.Debug(context, "Policy list: "+fmt.Sprintln(policyList), "permission")
 	if err != nil {
 		return false, errors.Wrap(err, "Error when fetching policy list for user:"+strconv.Itoa(userID))
 	}
-
 	limits := permission.GetLimitsFromPolicy(policyList, module, action)
 	debug.Debug(context, "Limits: "+fmt.Sprintln(limits), "permission")
 
-	matched := MatchLimits(limits, realData, context)
-	return matched, nil
-}
-
-//currentData example: {scope: "site1", under: [1,2,3,4,5], contenttype: "folder" }
-func MatchLimits(limits []map[string]interface{}, currentData map[string]interface{}, context context.Context) bool {
+	//match limits
 	result := false
 	for i, limit := range limits {
 		debug.Debug(context, "Matching limit "+strconv.Itoa(i+1)+"/"+strconv.Itoa(len(limits)), "permission")
-		policyResult, matchLog := util.MatchCondition(limit, currentData)
+		policyResult, matchLog := util.MatchCondition(limit, realData)
 		for _, item := range matchLog {
 			debug.Debug(context, item, "permission")
 		}
@@ -45,10 +41,10 @@ func MatchLimits(limits []map[string]interface{}, currentData map[string]interfa
 			break
 		}
 	}
-
-	return result
+	return result, nil
 }
 
+//If the use can read the content
 func CanRead(userID int, content contenttype.ContentTyper, context context.Context) (bool, error) {
 	location := content.GetLocation()
 	data := map[string]interface{}{
