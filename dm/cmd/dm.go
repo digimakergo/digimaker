66ec963565850b76fd015b1bb38e6284ebc9fa14
Chen Xiongjie <chen.xcmail@gmail.com>
2019-06-18 13:08:29 +0200

    Added: init dm cmd

---
+++ dm/cmd/dm.go
@@ -0,0 +1,19 @@
+package main
+
+//Generate database schema based on contenttype.json
+func GenerateSchema() {
+
+}
+
+func VerifySchema() {
+
+}
+
+//Generate entities based on contenttype.json
+func GenerateEnities() {
+
+}
+
+func main() {
+
+}
