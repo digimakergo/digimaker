dbda7071972a10d20e17717851de86f171a42fac
Chen Xiongjie <chen.xcmail@gmail.com>
2019-06-30 12:16:19 +0200

    Added: Start() in sitekit which can start from config file and also api

--- demosite/cmd/demo.go
+++ demosite/cmd/demo.go
@@ -3,7 +3,9 @@ package main
 import (
 	"dm/dm"
 	"dm/dm/handler"
+	"dm/dm/util"
 	"dm/niceurl"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -33,34 +35,96 @@ func BootStrap() {
 func main() {
 	BootStrap()
 
-	sitelist := []string{"demosite", "test"} //todo: load dynamically either from api or config file.
-	defaultSite := "demosite"
-
 	r := mux.NewRouter()
-	//route subsites
-	for _, site := range sitelist {
-		s := r.PathPrefix("/" + site + "/").Subrouter()
-		RouteContent(s, site, site)
+	config := util.GetConfigSectionAll("sites", "site").([]interface{})
+	for _, siteConfig := range config {
+		err := Start(r, siteConfig.(map[interface{}]interface{}))
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 	}
-	//route default site
-	RouteContent(r, defaultSite, "")
+	fmt.Println("success!")
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
 	http.Handle("/", r)
 	http.ListenAndServe(":8089", nil)
 }
 
-func RouteContent(r *mux.Router, site string, prefix string) {
+//start a site with configuration.
+//Config includes:
+// route:
+// template_folder
+// root:
+// default:
+func Start(r *mux.Router, config map[interface{}]interface{}) error {
+	routesConfig := config["routes"].([]interface{})
+
+	if _, ok := config["template_folder"]; !ok {
+		return errors.New("Need template_folder setting.")
+	}
+	tempalteFolder := util.InterfaceToStringArray(config["template_folder"].([]interface{}))
+
+	if _, ok := config["root"]; !ok {
+		return errors.New("Need root setting.")
+	}
+	root := config["root"].(int)
+
+	if _, ok := config["default"]; !ok {
+		return errors.New("Need default setting.")
+	}
+	defaultContent := config["default"].(int)
+	//go through all routes.
+	for _, routeConfig := range routesConfig {
+		var hosts []string
+		route := routeConfig.(map[interface{}]interface{})
+		if hostStr, ok := route["host"]; ok {
+			hosts = util.Split(hostStr.(string))
+		} else {
+			return errors.New("Need host setting.")
+		}
+
+		var paths []string
+		if value, ok := route["path"]; ok {
+			if value == "" {
+				paths = append(paths, "") //root
+			} else {
+				paths = util.Split(value.(string))
+			}
+		} else {
+			paths = append(paths, "") //root
+		}
+
+		//hosts and paths should AND match, but host in hosts(also path in paths) is OR.
+		for _, host := range hosts {
+			for _, path := range paths {
+				var s *mux.Router
+				//use subrouter which is better for performance
+				if path != "" {
+					s = r.Host(host).PathPrefix("/" + path + "/").Subrouter()
+				} else {
+					s = r.Host(host).Subrouter()
+				}
+				routeContent(s, tempalteFolder[0], path, root, defaultContent)
+			}
+		}
+	}
+
+	return nil
+}
+
+func routeContent(r *mux.Router, templateFolder string, prefix string, root int, defaultContent int) {
+	//todo: add route debug.
 	r.HandleFunc("/content/view/{id}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id, _ := strconv.Atoi(vars["id"])
-		viewContent(w, r, id, site, prefix) //todo: use default site.
+		viewContent(w, r, id, templateFolder, prefix)
 	})
 
 	r.MatcherFunc(niceurl.ViewContentMatcher).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id, _ := strconv.Atoi(vars["id"])
-		viewContent(w, r, id, site, prefix)
+		viewContent(w, r, id, templateFolder, prefix)
 	})
 }
 
