8da631e1aa46d2a7b24ff1d219b62f2fc716a261
Chen Xiongjie <chen.xcmail@gmail.com>
2019-08-11 19:29:54 +0200

    Updated: doc

--- rest/error.go
+++ rest/error.go
@@ -3,6 +3,7 @@ package rest
 import "net/http"
 
 func HandleError(err error, w http.ResponseWriter) {
+	//todo: output debug info if needed.
 	w.WriteHeader(500)    //todo: make it as parameter?
 	errStr := err.Error() //todo: use error code here
 	w.Write([]byte(errStr))
