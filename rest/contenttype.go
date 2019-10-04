e9fb1b959c60bf8277ac6017ac9887831a907b2d
Chen Xiongjie <chen.xcmail@gmail.com>
2019-10-04 17:54:04 +0200

    Added: get contenttype definition(contenttype/get/<identifier>)

--- rest/contenttype.go
+++ rest/contenttype.go
@@ -1,3 +1,22 @@
 //Author xc, Created on 2019-08-13 17:25
 //{COPYRIGHTS}
 package rest
+
+import (
+	"dm/core/contenttype"
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func GetDefinition(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	identifier := strings.TrimSpace(params["contentype"])
+
+	definition := contenttype.GetContentDefinition(identifier)
+	w.Header().Set("content-type", "application/json")
+	data, _ := json.Marshal(definition)
+	w.Write(data)
+}
--- rest/route.go
+++ rest/route.go
@@ -5,6 +5,7 @@ import (
 )
 
 func Route(r *mux.Router) {
+	r.HandleFunc("/contenttype/get/{contentype}", GetDefinition)
 	r.HandleFunc("/content/get/{id}", GetContent)
 	r.HandleFunc("/content/treemenu/{id}", TreeMenu)
 	r.HandleFunc("/content/list/{id}", Children)
