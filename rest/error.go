8167eb021901a57bd26ef5f4bf470c90376cd1bd
Chen Xiongjie <chen.xcmail@gmail.com>
2019-12-01 12:14:34 +0100

    Updated: can output error http code

--- rest/error.go
+++ rest/error.go
@@ -2,9 +2,13 @@ package rest
 
 import "net/http"
 
-func HandleError(err error, w http.ResponseWriter) {
+func HandleError(err error, w http.ResponseWriter, httpCode ...int) {
 	//todo: output debug info if needed.
-	w.WriteHeader(500)    //todo: make it as parameter?
+	if len(httpCode) == 0 {
+		w.WriteHeader(500)
+	} else {
+		w.WriteHeader(httpCode[0])
+	}
 	errStr := err.Error() //todo: use error code here
 	w.Write([]byte(errStr))
 }
