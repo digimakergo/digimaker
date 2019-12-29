3dd33ed77796947c7f7ed45b19ac5ceb24ba1ee6
Chen Xiongjie <chen.xcmail@gmail.com>
2019-12-29 17:44:39 +0100

    Updated: merge all permission file into one file

--- core/permission/policy.go
+++ core/permission/policy.go
@@ -5,8 +5,21 @@
 package permission
 
 import (
+	"context"
+	"dm/core/contenttype"
+	"dm/core/db"
 	"dm/core/util"
+	"dm/core/util/debug"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
 )
+
+/*************
+Policy & Permission
+*************/
 
 type Permission struct {
 	Operation []string               `json:"operation"`
@@ -32,4 +45,117 @@ func LoadPolicies() error {
 
 func GetPolicy(identifier string) Policy {
 	return policyDefinition[identifier]
+}
+
+/*************
+User role
+*************/
+type UserRole struct {
+	ID     int `boil:"id" json:"id" toml:"id" yaml:"id"`
+	UserID int `boil:"user_id" json:"user_id" toml:"user_id" yaml:"user_id"`
+	RoleID int `boil:"role_id" json:"role_id" toml:"role_id" yaml:"role_id"`
+}
+
+func GetUserPolicies(userID int) ([]RolePolicy, error) {
+	//get usergroups
+	dbHandler := db.DBHanlder()
+
+	list := []UserRole{}
+	err := dbHandler.GetEntity("dm_user_role", db.Cond("user_id", userID), []string{}, &list)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can not get user role by user id: "+strconv.Itoa(userID))
+	}
+	//get permissions
+	policyList := []RolePolicy{}
+	for _, userRole := range list {
+		currentPolicyList, err := GetPermissions(userRole.RoleID)
+		if err != nil {
+			return nil, errors.Wrap(err, "Can not get permission on usergroup: "+strconv.Itoa(userRole.RoleID))
+		}
+		for _, policy := range currentPolicyList {
+			policyList = append(policyList, policy)
+		}
+	}
+	return policyList, nil
+}
+
+//Get user's limits
+func GetUserLimits(userID int, operation string, context context.Context) ([]map[string]interface{}, error) {
+	policyList, err := GetUserPolicies(userID)
+	debug.Debug(context, "Got policy list: "+fmt.Sprint(policyList), "permission")
+	if err != nil {
+		return nil, errors.Wrap(err, "Error when fetching policy list for user:"+strconv.Itoa(userID))
+	}
+	//todo: cache limits to user, and cache anoymous globally.
+	result := GetLimitsFromPolicy(policyList, operation)
+	debug.Debug(context, "Got limits:"+fmt.Sprint(result), "permission")
+	return result, nil
+}
+
+func GetLimitsFromPolicy(policyList []RolePolicy, operation string) []map[string]interface{} {
+	result := []map[string]interface{}{}
+	for _, ugPolicy := range policyList {
+		policy := ugPolicy.GetPolicy()
+		for _, permission := range policy.Permissions {
+			for _, item := range permission.Operation {
+				if item == operation {
+					limit := permission.LimitedTo
+					if ugPolicy.Scope != "" {
+						limit["scope"] = ugPolicy.Scope
+					}
+					if ugPolicy.Under != "" {
+						limit["under"] = ugPolicy.Under
+					}
+					result = append(result, limit)
+				}
+			}
+		}
+	}
+	return result
+}
+
+/*************
+Role policy
+*************/
+type RolePolicy struct {
+	ID     int    `boil:"id" json:"id" toml:"id" yaml:"id"`
+	RoleID string `boil:"role_id" json:"role_id" toml:"role_id" yaml:"role_id"`
+	Policy string `boil:"policy" json:"policy" toml:"policy" yaml:"policy"`
+	Under  string `boil:"under" json:"under" toml:"under" yaml:"under"`
+	Scope  string `boil:"scope" json:"scope" toml:"scope" yaml:"scope"`
+	policy Policy `boil:"-"` //cache for Policy instance
+}
+
+//Get policy detail of current usergroup policy.
+func (rp RolePolicy) GetPolicy() Policy {
+	if len(rp.policy.Permissions) == 0 {
+		rp.policy = GetPolicy(rp.Policy)
+	}
+	return rp.policy
+}
+
+//Get UsergroupPolicy slice based on usergroupID including inhertated permissions.
+func GetPermissions(usergroupID int) ([]RolePolicy, error) {
+	dbHandler := db.DBHanlder()
+	location := contenttype.Location{}
+	//todo: maybe better to use content id
+	err := dbHandler.GetEntity("dm_location", db.Cond("id", usergroupID), []string{}, &location) //note: use this instead of handler.Querier() to avoid cycle dependency because handler package rely on permission
+	if err != nil {
+		fmt.Println(err) //todo: make it generic
+	}
+
+	hierarchy := location.Hierarchy
+	ids := strings.Split(hierarchy, "/")
+
+	roleIDs := []int{}
+	for _, item := range ids {
+		itemInt, _ := strconv.Atoi(item)
+		roleIDs = append(roleIDs, itemInt)
+	}
+	policyList := []RolePolicy{}
+	err = dbHandler.GetEntity("dm_role_policy", db.Cond("role_id", roleIDs), []string{}, &policyList)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can not fetch dm_usergroup_policy. usergroup_id :"+strings.Join(ids, ","))
+	}
+	return policyList, nil
 }
