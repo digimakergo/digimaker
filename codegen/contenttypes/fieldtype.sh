4cc061fd3765ca4b38349dca39445f2dbed7017d
Chen Xiongjie <chen.xcmail@gmail.com>
2020-05-11 23:10:57 +0200

    Added: fieldtype.sh to generate 3rd party registry before generating entities

---
+++ codegen/contenttypes/fieldtype.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+package=$1
+if test -z "$package"
+then
+  echo "Usage: fieldtype.sh <project package name>"
+  exit
+fi
+dir=`dirname $0`
+if [ ! -d "${dir}/temp" ]
+then
+echo `mkdir ${dir}/temp`
+fi
+echo  "package temp
+import _ \"${package}/fieldtype\"" > ./$dir/temp/project.go
