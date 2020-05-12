1613c799275984fbe1045bd2156bc925aea3c1d4
Chen Xiongjie <chen.xcmail@gmail.com>
2020-05-12 10:18:27 +0200

    Updated: codegen not to generate loader when there is no project fieldtype

--- codegen/contenttypes/fieldtype_loader.sh
+++ codegen/contenttypes/fieldtype_loader.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
+# This script is to generate a project.go under temp which imports project fieldtype package only
 package=$1
 if test -z "$package"
 then
   echo "Usage: fieldtype.sh <project package name>"
   exit
 fi
+
+if [ ! -d "src/$package/fieldtype" ]; then
+    echo "No project field type found"
+    exit
+fi
+
 dir=`dirname $0`
 if [ ! -d "${dir}/temp" ]
 then
--- codegen/contenttypes/gen.sh
+++ codegen/contenttypes/gen.sh
@@ -9,4 +9,6 @@ dir=`dirname $0`
 path=$1
 sh $dir/fieldtype_loader.sh $path
 go run $dir/gen.go src/$path
-rm $dir/temp/project.go
+if [ -f "$dir/temp/project.go" ]; then
+  rm $dir/temp/project.go
+fi
