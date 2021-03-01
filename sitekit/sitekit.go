2ef1b4de7eb3106adde7557c0fea278bd09485c6
Chen Xiongjie <chen.xcmail@gmail.com>
2021-03-01 13:28:50 +0100

    Updated: restructure sitekit

--- sitekit/sitekit.go
+++ sitekit/sitekit.go
@@ -19,6 +19,31 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var siteSettings = map[string]SiteSettings{}
+var siteIdentifiers = []string{}
+
+//a basic setting to run a site.
+type SiteSettings struct {
+	TemplateBase    string
+	TemplateFolders []string
+	RootContent     contenttype.ContentTyper
+	DefaultContent  contenttype.ContentTyper
+	Routes          []interface{} //host, path.
+}
+
+func GetSiteSettings(identifier string) SiteSettings {
+	return siteSettings[identifier]
+}
+
+func GetSites() []string {
+	return siteIdentifiers
+}
+
+func SetSiteSettings(identifier string, settings SiteSettings) {
+	siteSettings[identifier] = settings
+	siteIdentifiers = append(siteIdentifiers, identifier)
+}
+
 //Initialize all the sites
 func InitSite(r *mux.Router, siteConfig map[string]interface{}) error {
 
