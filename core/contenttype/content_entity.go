8932fa3d310ab98e79fd40d468290c879dd9e09f
Chen Xiongjie <chen.xcmail@gmail.com>
2021-09-24 15:03:34 +0200

    added: author to non-location content

--- core/contenttype/content_entity.go
+++ core/contenttype/content_entity.go
@@ -5,6 +5,7 @@ type ContentEntity struct {
 	ContentType string `boil:"content_type" json:"content_type" toml:"content_type" yaml:"content_type"`
 	CID         int    `boil:"cid" json:"cid" toml:"cid" yaml:"cid"`
 	//todo: may put more here like Author, LocationID
+	Author     int                 `boil:"author" json:"author" toml:"author" yaml:"author"`
 	AuthorName string              `boil:"author_name" json:"author_name" toml:"author_name" yaml:"author_name"`
 	Relations  ContentRelationList `boil:"relations" json:"relations" toml:"relations" yaml:"relations"`
 }
