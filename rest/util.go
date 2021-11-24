a263978fdfa122f91a13be8a6e899361bef70f21
Chen Xiongjie <chen.xcmail@gmail.com>
2021-11-24 12:23:24 +0100

    updated: remove built-in pdf feature

--- rest/util.go
+++ rest/util.go
@@ -6,20 +6,14 @@ package rest
 import (
 	"errors"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
-	"os/exec"
 	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/digimakergo/digimaker/core/permission"
-	"github.com/digimakergo/digimaker/core/query"
 	"github.com/digimakergo/digimaker/core/util"
 
 	"github.com/gorilla/mux"
-	pongo2 "gopkg.in/flosch/pongo2.v2"
 )
 
 func UploadFile(w http.ResponseWriter, r *http.Request) {
@@ -107,80 +101,6 @@ func HandleUploadFile(r *http.Request, filetype string) (string, error) {
 	return tempFolder + "/" + tempFilename, nil
 }
 
-func ExportPDF(w http.ResponseWriter, r *http.Request) {
-	userID := CheckUserID(r.Context(), w)
-	if userID == 0 {
-		return
-	}
-
-	params := mux.Vars(r)
-	id := params["id"]
-	language := r.FormValue("language")
-	if language == "" {
-		language = "default"
-	}
-
-	idInt, err := strconv.Atoi(id)
-	if err != nil {
-		HandleError(errors.New("id not int"), w)
-		return
-	}
-	content, err := query.FetchByID(r.Context(), idInt)
-	if err != nil {
-		HandleError(errors.New("Content not found"), w)
-		return
-	}
-	if !permission.CanRead(r.Context(), userID, content) {
-		HandleError(errors.New("Need permission"), w)
-		return
-	}
-
-	tpl := pongo2.Must(pongo2.FromFile(util.AbsHomePath() + "/templates/pdf/main.html"))
-	variables := map[string]interface{}{}
-	variables["language"] = language
-	variables["content"] = content
-	authorID := content.Value("author").(int)
-	author, _ := query.FetchByCID(r.Context(), "user", authorID)
-	variables["author"] = author
-
-	data, err2 := tpl.ExecuteBytes(pongo2.Context(variables))
-	if err2 != nil {
-		HandleError(err2, w)
-		return
-	}
-
-	modified := content.Value("modified").(int)
-	name := util.NameToIdentifier(content.GetName()) + "-" + id + "-" + language + "-" + strconv.Itoa(modified)
-	pdfFile, err := HtmlToPDF(string(data), name)
-	if err != nil {
-		HandleError(err, w)
-		return
-	}
-
-	w.Write([]byte(pdfFile))
-}
-
-func HtmlToPDF(html string, name string) (string, error) {
-	tempFolder := util.GetConfig("general", "var_folder", "dm")
-	targetName := "/pdf/" + name + ".pdf"
-	target := tempFolder + targetName
-
-	//if exist already, return it
-	if _, err := os.Stat(target); err == nil {
-		return targetName, nil
-	}
-	sourceName := "/pdf/" + name + ".html"
-
-	source := tempFolder + sourceName
-	ioutil.WriteFile(source, []byte(html), 0777)
-	output, _ := exec.Command("wkhtmltopdf", "--javascript-delay", "1500", "-L", "0px", "-R", "0px", "-T", "95px", "-B", "95.5px", "--print-media-type", "--header-html", tempFolder+"/pdf-assets/header.html", "--footer-html", tempFolder+"/pdf-assets/footer.html", source, target).Output()
-	log.Println(output)
-	// if err != nil {
-	// 	return "", err
-	// }
-	return targetName, nil
-}
-
 func GetAllowedLimitations(w http.ResponseWriter, r *http.Request) {
 	userId := CheckUserID(r.Context(), w)
 	if userId == 0 {
@@ -209,6 +129,4 @@ func init() {
 	RegisterRoute("/util/uploadfile", UploadFile, "POST")
 	RegisterRoute("/util/uploadimage", UploadImage, "POST")
 	RegisterRoute("/util/limitations/{operation}", GetAllowedLimitations)
-
-	RegisterRoute("/pdf/{id}", ExportPDF)
 }
