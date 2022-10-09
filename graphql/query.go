f4ae20e34af492c5d819a85ad15a9bf61f82520c
powerless <asphalt1018@gmail.com>
2022-10-09 14:09:55 +0800

    added: func

--- graphql/query.go
+++ graphql/query.go
@@ -1,11 +1,13 @@
 package graphql
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/digimakergo/digimaker/core/contenttype"
 	"github.com/digimakergo/digimaker/core/db"
+	"github.com/digimakergo/digimaker/core/log"
 	"github.com/digimakergo/digimaker/core/query"
 	"github.com/digimakergo/digimaker/rest"
 	"github.com/graphql-go/graphql"
@@ -104,67 +106,10 @@ func QueryGraphql(w http.ResponseWriter, r *http.Request) {
 
 							queryType.AddFieldConfig(cType, &graphql.Field{
 								Type: graphql.NewList(cFieldOfType),
-								Args: genRootArgs(args), // return a config
+								Args: buildRootArgs(args), // return a config
 								Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-									// todo : generate query params
-									var list interface{}
-									var lerr error
-									condition := db.Condition{}
-									// filters params
-									fmt.Println("args:", p.Args)
-
-									if len(p.Args) > 0 {
-										// conditon key query
-										for k, v := range p.Args {
-											if k == "filter" || k == "sort" || k == "limit" || k == "offset" {
-												continue
-											}
-											list, _, lerr := query.List(ctx, cType, db.Cond(k, v))
-											return list, lerr
-										}
-										if filter, ok := p.Args["filter"]; ok {
-											fmt.Println("filter params:", buildJsonResult(filter))
-											if filterMap, mapOk := filter.(map[string]interface{}); mapOk {
-												if andMap, andOk := filterMap["and"].(map[string]interface{}); andOk {
-													fmt.Println("andMap params:", buildJsonResult(andMap))
-													if andMap != nil && len(andMap) > 0 {
-														for k, v := range andMap {
-															if k == "cid" {
-																k = "id"
-															}
-															condition = condition.And(k, v)
-														}
-													}
-												}
-												if orMap, orOk := filterMap["or"].(map[string]interface{}); orOk {
-													fmt.Println("orMap params:", buildJsonResult(orMap))
-													if orMap != nil && len(orMap) > 0 {
-														for k, v := range orMap {
-															condition = condition.Or(k, v)
-														}
-													}
-												}
-											}
-										}
-
-										// sort params
-										if sorts, ok := p.Args["sort"].([]interface{}); ok {
-											fmt.Println("sort args:", buildJsonResult(sorts))
-											sortPs := make([]string, 0)
-											for _, i := range sorts {
-												sortPs = append(sortPs, fmt.Sprint(i))
-											}
-											condition = condition.Sortby(sortPs...)
-										}
-
-										// page params
-										condition = condition.Limit(p.Args["offset"].(int), p.Args["limit"].(int))
-									}
-
-									conditionRet, conditionParams := db.BuildCondition(condition)
-									fmt.Println("condition:", conditionRet, "|", conditionParams, "|", condition)
-									list, _, lerr = query.List(ctx, cType, condition)
-									return list, lerr
+									log.Info(p)
+									return parseSolveParams(ctx, cType, p)
 								},
 							})
 						}
@@ -187,7 +132,7 @@ func QueryGraphql(w http.ResponseWriter, r *http.Request) {
 	rest.WriteResponse(result, w)
 }
 
-func genRootArgs(content graphql.FieldConfigArgument) (ret graphql.FieldConfigArgument) {
+func buildRootArgs(content graphql.FieldConfigArgument) (ret graphql.FieldConfigArgument) {
 	inputArgs := make(graphql.InputObjectConfigFieldMap, 0)
 	sortArgs := make(graphql.InputObjectConfigFieldMap, 0)
 	rootArgs := make(graphql.FieldConfigArgument, 0)
@@ -207,40 +152,20 @@ func genRootArgs(content graphql.FieldConfigArgument) (ret graphql.FieldConfigAr
 		}
 	}
 
-	// conditon args
-	conditionArgs := graphql.InputObjectConfigFieldMap{
-		"and": &graphql.InputObjectFieldConfig{
-			Type: graphql.NewInputObject(graphql.InputObjectConfig{
-				Name:        "and args",
-				Fields:      inputArgs,
-				Description: "and args",
-			}),
-		},
-		"or": &graphql.InputObjectFieldConfig{
-			Type: graphql.NewInputObject(graphql.InputObjectConfig{
-				Name:        "or args",
-				Fields:      inputArgs,
-				Description: "or args",
-			}),
-		},
-	}
+	// build conditon args
+	conditionArgs := buildConditionArgs(inputArgs)
 
 	// root args add children args
 	// filter query
 	rootArgs["filter"] = &graphql.ArgumentConfig{
 		Type: graphql.NewInputObject(graphql.InputObjectConfig{
-			Name:        "filter args",
+			Name:        "FILTER",
 			Fields:      conditionArgs,
 			Description: "filter args",
 		}),
 	}
 	// sort query : sort:{cid:"desc"}
 	rootArgs["sort"] = &graphql.ArgumentConfig{
-		//Type: graphql.NewInputObject(graphql.InputObjectConfig{
-		//	Name:        "sort args",
-		//	Fields:      sortArgs,
-		//	Description: "sort args",
-		//}),
 		Type:        graphql.NewList(graphql.String),
 		Description: "sort args",
 	}
@@ -260,10 +185,182 @@ func genRootArgs(content graphql.FieldConfigArgument) (ret graphql.FieldConfigAr
 	return rootArgs
 }
 
+func buildConditionArgs(inputArgs graphql.InputObjectConfigFieldMap) (conditionArgs graphql.InputObjectConfigFieldMap) {
+	conditionArgs = graphql.InputObjectConfigFieldMap{
+		"and": &graphql.InputObjectFieldConfig{
+			Type: graphql.NewInputObject(graphql.InputObjectConfig{
+				Name:        "AND",
+				Fields:      inputArgs,
+				Description: "and args",
+			}),
+		},
+		"or": &graphql.InputObjectFieldConfig{
+			Type: graphql.NewInputObject(graphql.InputObjectConfig{
+				Name:        "OR",
+				Fields:      inputArgs,
+				Description: "or args",
+			}),
+		},
+		"gt": &graphql.InputObjectFieldConfig{
+			Type: graphql.NewInputObject(graphql.InputObjectConfig{
+				Name:        "GT",
+				Fields:      inputArgs,
+				Description: "gt args (>)",
+			}),
+		},
+		"ge": &graphql.InputObjectFieldConfig{
+			Type: graphql.NewInputObject(graphql.InputObjectConfig{
+				Name:        "GE",
+				Fields:      inputArgs,
+				Description: "gt args (>=)",
+			}),
+		},
+		"lt": &graphql.InputObjectFieldConfig{
+			Type: graphql.NewInputObject(graphql.InputObjectConfig{
+				Name:        "LT",
+				Fields:      inputArgs,
+				Description: "lt args (<)",
+			}),
+		},
+		"le": &graphql.InputObjectFieldConfig{
+			Type: graphql.NewInputObject(graphql.InputObjectConfig{
+				Name:        "LE",
+				Fields:      inputArgs,
+				Description: "le args (<=)",
+			}),
+		},
+	}
+	return conditionArgs
+}
+
+func isConditionKey(key string) bool {
+	params := map[string]bool{
+		"and":    true,
+		"or":     true,
+		"gt":     true,
+		"ge":     true,
+		"lt":     true,
+		"le":     true,
+		"limit":  true,
+		"offset": true,
+		"filter": true,
+	}
+	if v, ok := params[key]; ok {
+		return v
+	}
+	return false
+}
+
+func verifyKey(key string) bool {
+	switch key {
+	case "and", "or", "gt", "ge", "lt", "le":
+		return true
+	case "filter":
+		return true
+	case "limit", "offset":
+		return true
+	default:
+		return false
+	}
+}
+
+func generateFilterArgs(filterMap map[string]interface{}, condition db.Condition) db.Condition {
+	if andMap, ok := filterMap["and"].(map[string]interface{}); ok {
+		if andMap != nil && len(andMap) > 0 {
+			for k, v := range andMap {
+				//condition = condition.And(k, v)
+				condition = condition.And(k, v)
+			}
+		}
+	}
+	if orMap, ok := filterMap["or"].(map[string]interface{}); ok {
+		if orMap != nil && len(orMap) > 0 {
+			for k, v := range orMap {
+				//condition = condition.Or(k, v)
+				condition = condition.Or(k, v)
+			}
+		}
+	}
+	if gtMap, ok := filterMap["gt"].(map[string]interface{}); ok {
+		if gtMap != nil && len(gtMap) > 0 {
+			for k, v := range gtMap {
+				condition = condition.Cond(k+" >", v)
+			}
+		}
+	}
+
+	if geMap, ok := filterMap["ge"].(map[string]interface{}); ok {
+		if geMap != nil && len(geMap) > 0 {
+			for k, v := range geMap {
+				condition = condition.Cond(k+" >=", v)
+			}
+		}
+	}
+
+	if ltMap, ok := filterMap["lt"].(map[string]interface{}); ok {
+		if ltMap != nil && len(ltMap) > 0 {
+			for k, v := range ltMap {
+				//condition = condition.Or(k, v)
+				condition = condition.Cond(k+" <", v)
+			}
+		}
+	}
+
+	if leMap, ok := filterMap["le"].(map[string]interface{}); ok {
+		if leMap != nil && len(leMap) > 0 {
+			for k, v := range leMap {
+				//condition = condition.Or(k, v)
+				condition = condition.Cond(k+" <=", v)
+			}
+		}
+	}
+	return condition
+}
+
+func parseSolveParams(ctx context.Context, cType string, p graphql.ResolveParams) (list interface{}, err error) {
+
+	if p.Args == nil || len(p.Args) == 0 {
+		return list, errors.New("args is nil")
+	}
+
+	condition := db.Condition{}
+	// condition key query
+	for k, v := range p.Args {
+		// verify key
+		if isConditionKey(k) {
+			continue
+		}
+		condition = condition.Cond(k, v)
+		list, _, err = query.List(ctx, cType, condition)
+		return list, err
+	}
+	if filter, ok := p.Args["filter"]; ok {
+		if filterMap, ok := filter.(map[string]interface{}); ok {
+			condition = generateFilterArgs(filterMap, condition)
+		}
+	}
+
+	// sort params
+	if sorts, ok := p.Args["sort"].([]interface{}); ok {
+		sortPs := make([]string, 0)
+		for _, i := range sorts {
+			sortPs = append(sortPs, fmt.Sprint(i))
+		}
+		condition = condition.Sortby(sortPs...)
+	}
+
+	// page params
+	condition = condition.Limit(p.Args["offset"].(int), p.Args["limit"].(int))
+	conRet, conParams := db.BuildCondition(condition)
+	log.Info(fmt.Sprintln("build condition:", conRet, "|", conParams, "|", condition))
+	list, _, err = query.List(ctx, cType, condition)
+	return list, err
+}
+
 func buildJsonResult(input interface{}) string {
 	ret, err := json.Marshal(input)
 	if err != nil {
-		return "{\"data\":" + err.Error() + "}"
+		return "{\"data\": \"" + err.Error() + "\"}"
 	}
 	return string(ret)
 }
