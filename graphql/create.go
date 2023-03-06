4347270e3358b9dc8b00c88500715f422896271c
William <williamlsh@protonmail.com>
2023-03-06 22:59:01 +0000

    Add creating initial version

--- graphql/create.go
+++ graphql/create.go
@@ -15,42 +15,29 @@ import (
 	"github.com/graphql-go/graphql/language/ast"
 )
 
-func mutationType() *graphql.Object {
-	gqlContentTypes := graphql.Fields{}
-	for cType, def := range definition.GetDefinitionList()["default"] {
-		contentGQLType := getContentGQLType(def)
-		buildUpdateType(gqlContentTypes, cType, def, contentGQLType)
-		buildCreateType(gqlContentTypes, cType, def, contentGQLType)
-	}
-	return graphql.NewObject(graphql.ObjectConfig{
-		Name:   "Mutation",
-		Fields: gqlContentTypes,
-	})
-}
-
 // For the following schema:
 //
-//	mutation update {
-//	  updateArticle(updateData: [{data: {title: "", summary: ""}, id: 1}]) {
-//	    id
-//	    title
-//	  }
+//	mutation {
+//		createArticle(createData: [{data: {title: "", body: ""}, parent: 3}]) {
+//		  id
+//		  title
+//		}
 //	}
-func buildUpdateType(gqlContentTypes graphql.Fields, cType string, def definition.ContentType, contentGQLType *graphql.Object) {
-	cType = "update" + util.UpperName(cType)
+func buildCreateType(gqlContentTypes graphql.Fields, cType string, def definition.ContentType, contentGQLType *graphql.Object) {
+	cType = "create" + util.UpperName(cType)
 	gqlContentTypes[cType] = &graphql.Field{
 		Name: def.Name,
 		Type: graphql.NewList(contentGQLType),
-		Args: buildUpdateArgs(cType),
+		Args: buildCreateArgs(cType),
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-			return resolveUpdateMutation(p.Context, p)
+			return resolveCreateMutation(p.Context, p)
 		},
 	}
 }
 
-func buildUpdateArgs(cType string) graphql.FieldConfigArgument {
+func buildCreateArgs(cType string) graphql.FieldConfigArgument {
 	result := make(graphql.FieldConfigArgument)
-	result["updateData"] = &graphql.ArgumentConfig{
+	result["createData"] = &graphql.ArgumentConfig{
 		Type: graphql.NewScalar(graphql.ScalarConfig{
 			Name:        "DataInput",
 			Description: "Data input type.",
@@ -64,39 +51,39 @@ func buildUpdateArgs(cType string) graphql.FieldConfigArgument {
 				switch v := valueAST.(type) {
 				case *ast.ListValue:
 					def, _ := definition.GetDefinition(cType)
-					result := make([]updateInputData, len(v.Values))
+					result := make([]createInputData, len(v.Values))
 					for i, v := range v.Values {
 						if object, ok := v.(*ast.ObjectValue); ok {
-							updateData := updateInputData{data: make(handler.InputMap)}
-							if err := parseUpdateInput(object.Fields, &updateData, def.FieldMap); err != nil {
+							createData := createInputData{data: make(handler.InputMap)}
+							if err := parseCreateInput(object.Fields, &createData, def.FieldMap); err != nil {
 								return fmt.Errorf("failed to parse input data: %w", err)
 							}
-							result[i] = updateData
+							result[i] = createData
 						}
 					}
 					return result
 				default:
-					return errors.New("unknown updateData")
+					return errors.New("unknown createData")
 				}
 			},
 		}),
-		Description: "Data to be updated.",
+		Description: "Data to be Created.",
 	}
 	return result
 }
 
-func resolveUpdateMutation(ctx context.Context, p graphql.ResolveParams) (interface{}, error) {
+func resolveCreateMutation(ctx context.Context, p graphql.ResolveParams) (interface{}, error) {
 	var result []contenttype.ContentTyper
-	if inputs, ok := p.Args["updateData"]; ok {
+	if inputs, ok := p.Args["createData"]; ok {
 		switch v := inputs.(type) {
-		case []updateInputData:
+		case []createInputData:
 			result = make([]contenttype.ContentTyper, len(v))
 			for i, item := range v {
 				userId := util.CurrentUserID(ctx)
 				if userId == 0 {
 					return nil, errors.New("need to login")
 				}
-				content, err := handler.UpdateByContentID(ctx, p.Info.FieldName, item.id, item.data, userId)
+				content, err := handler.Create(ctx, userId, p.Info.FieldName, item.data, item.parentID)
 				if err != nil {
 					return nil, fmt.Errorf("could not update content by id: %w", err)
 				}
@@ -105,22 +92,22 @@ func resolveUpdateMutation(ctx context.Context, p graphql.ResolveParams) (interf
 		case error:
 			return nil, v
 		default:
-			return nil, errors.New("could not resolve, unknown updateData")
+			return nil, errors.New("could not resolve, unknown createData")
 		}
 	}
 	return result, nil
 }
 
-func parseUpdateInput(fields []*ast.ObjectField, input *updateInputData, fieldMap map[string]fieldtype.FieldDef) error {
+func parseCreateInput(fields []*ast.ObjectField, input *createInputData, fieldMap map[string]fieldtype.FieldDef) error {
 	for _, field := range fields {
 		key := field.Name.Value
 		value := field.Value.GetValue()
-		if key == "id" {
-			id, err := strconv.Atoi(value.(string))
+		if key == "parent" {
+			parentID, err := strconv.Atoi(value.(string))
 			if err != nil {
-				return fmt.Errorf("could not convert id string to int: %w", err)
+				return fmt.Errorf("could not convert parent id string to int: %w", err)
 			}
-			input.id = id
+			input.parentID = parentID
 		}
 		if key == "data" {
 			if fields, ok := value.([]*ast.ObjectField); ok {
@@ -140,8 +127,7 @@ func parseUpdateInput(fields []*ast.ObjectField, input *updateInputData, fieldMa
 	return nil
 }
 
-// updateInputData is user input data.
-type updateInputData struct {
-	id   int
-	data handler.InputMap
+type createInputData struct {
+	parentID int
+	data     handler.InputMap
 }
